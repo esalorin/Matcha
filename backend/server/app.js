0f18706ee82a9c3681c292ee89a4ff1fa5cfd69a
kmoilane <karri.moilanen@gmail.com>
2021-12-07 12:48:27 +0200

    added sessionStore

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,17 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-
 const userRouter = require("../routes/userRoutes");
-
 const { response } = require("express");
+const sessionStore = require("../config/sessionStore");
 
 const app = express();
-
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -21,23 +18,20 @@ app.use(cors({
 	credentials: true
 
 }));
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true}));
-
 app.use(
 	session({
 		key: "userId",
-		secret: "startmatching",
+		secret: "Matcha dating site made by eena & karri and some eXtra safety 4109248OAFsdfjf94fa",
+		store: sessionStore,
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
-			expires: 24 * 60 * 60 * 1000,
+			maxAge: 24 * 60 * 60 * 1000,
 		},
 	})
 );
-
-var nodemailer = require('nodemailer');
 
 app.use("/user", userRouter);
 
