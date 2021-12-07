7a9ca10a4d7427e55ac5a46d03737a184ef729ab
kmoilane <karri.moilanen@gmail.com>
2021-12-07 12:51:08 +0200

    Attempt of conditional rendering (not working atm)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {Header} from './components';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Verify from './pages/Verify';
-import Chat from './pages/Chat';
+//import Chat from './pages/Chat';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
-import Browse from './pages/Browse';
+//import Browse from './pages/Browse';
 import './App.css';
 
 const App = () => {
-  return (
-	<div className="App">
-		<Router>
-			<Header />
-			<Routes>
-				<Route exact path='/' element={<Home/>}/>
-				<Route exact path='/user/login' element={<Login/>}/>
-				<Route exact path='/user/register' element={<Register/>}/>
-				<Route exact path='/user/verify' element={<Verify/>}/>
-				{/* This when logged in:
-				<Route exact path='/' element={<Browse/>}/>
-				<Route exact path='/chat' element={<Chat/>}/>
-				<Route exact path='/profile' element={<Profile/>}/>*/}
-			</Routes>
-		</Router>
-	</div>
-)}
+
+	const [ loggedIn, setLoggedIn] = useState(false);
+
+	useEffect(() => {
+		axios.get("http://localhost:3001/user/auth")
+		.then((res) => {
+			if (res.data.loggedIn === true) {
+				setLoggedIn(true);
+			}
+		});
+	}, [setLoggedIn]);
+
+	if (loggedIn){
+		return (
+			<div className="App">
+				<Router>
+					<Header />
+					<Routes>
+						<Route exact path='/' element={<Home/>}/>
+						{/* This when logged in:
+						<Route exact path='/' element={<Browse/>}/>
+						<Route exact path='/chat' element={<Chat/>}/>*/
+						<Route exact path='/profile' element={<Profile/>}/>}
+					</Routes>
+				</Router>
+			</div>
+		);
+	}
+
+	else {
+		return (
+			<div className="App">
+				<Router>
+					<Header />
+					<Routes>
+						<Route exact path='/' element={<Home/>}/>
+						<Route exact path='/user/login' element={<Login/>}/>
+						<Route exact path='/user/register' element={<Register/>}/>
+						<Route exact path='/user/verify' element={<Verify/>}/>
+					</Routes>
+				</Router>
+			</div>
+		);
+	}
+}
 
 export default App;
