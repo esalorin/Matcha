4f7191b9c9d71cdcd2d252daa95c37503b082cec
Eena Salorinta <eenasalorinta@Eena-MacBook-Pro.local>
2021-12-16 15:36:21 +0200

    added locations selection to profile_setup_form with google maps API

--- frontend/src/components/forms/profile_setup_form/MatchingInfo.js
+++ frontend/src/components/forms/profile_setup_form/MatchingInfo.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng} from 'react-places-autocomplete';
+
 
 function MatchingInfo() {
+	const [address, setAddress] = useState("");
+	const [coordinates, setCoordinates] = useState({
+		lat: null,
+		lng: null
+	});
+
+	const handleInputChange = (event) => {
+		setAddress(event);
+	}
+	
+	const handleSelect = async (value) => {
+		const result = await geocodeByAddress(value);
+		const latLng  = await getLatLng(result[0]);
+		setAddress(value);
+		setCoordinates(latLng);
+		console.log(latLng);
+		console.log(value);
+
+	}
+
 	return (
 		<div>
 			<label for="age">Enter your age: </label>
@@ -21,6 +43,32 @@ function MatchingInfo() {
 				<option value="bisexual">None of your business</option>
 			</select>
 			<br/>
+			<br/>
+			<label for="location">Enter your location (optional): </label>
+			<PlacesAutocomplete value={address} onChange={(event) => handleInputChange(event)} onSelect={handleSelect}>
+				{({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+					<div className="location-div">
+						<input {...getInputProps({placeholder: "Search Places ..."})}className="form-input" name="location" id="location" type="text"></input>
+						<div className="locations-dropdown">
+							{loading ? <div>...loading</div>:null}
+
+							{suggestions.map((suggestion) => {
+								const style = {
+									backgroundColor: suggestion.active ? "#FF5B6F": "white",
+									textAlign: "center",
+									minHeight:"30px",
+									borderBottom: "1px solid rgb(238, 238, 238)",
+									cursor: "pointer"
+								};
+
+								return (<div {...getSuggestionItemProps(suggestion, {style})}>
+									{suggestion.description}
+									</div>)
+							})}
+						</div>
+					</div>
+				)}
+			</PlacesAutocomplete>
 		</div>
 	);
 }
